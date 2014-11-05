05364969321135202b0af45f3515868b98a93f9f
Snehasish Kumar <ska124@sfu.ca>
2014-11-05 01:12:08 -0800

    Added SHOC

---
+++ Workloads/SHOC/reduction/reduction.c
@@ -0,0 +1,34 @@
+#include "reduction.h"
+
+int in[size];
+
+int reduction()
+{
+  int i = 0;
+  int sum = 0;
+
+  for (i = 0; i < size; i++)
+    sum += in[i];
+  
+  return sum;
+}
+
+
+int main()
+{
+  /*int *in;*/
+  /*in = (int *) malloc (sizeof(int) * size );*/
+
+  int i;
+  int max = 2147483646;
+  int min = 0;
+  srand(8650341L);
+  for (i = 0; i < size; i++)
+  {
+    in[i] = (int)(min + rand() * 1.0 * (max - min) / (RAND_MAX ));
+  }
+
+  int sum = reduction();
+  printf("sum: %d\n", sum);
+  return 0;
+}
