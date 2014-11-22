5f94ccfa638f0cf2cbd4b3f63b91c16860865a85
Naveen Vedula <nvedula@sfu.ca>
2014-11-22 11:51:22 -0800

    traces for b1 to b4 for adpcm added and Run Folder for same

---
+++ Traces/small/b4/tracking/modify_trace_b4.sh
@@ -0,0 +1,13 @@
+for x in {1..7}; do
+    if [ ! -f "trace_${x}.raw" ]; then
+        touch trace_${x}.raw
+        gzip trace_${x}.raw
+        mv trace_${x}.raw.gz trace_${x}.raw
+    fi
+done
+
+cp ../trace.txt .
+ln -s ../../../../tools/post-b4/process 
+./process 4096
+rm process
+
