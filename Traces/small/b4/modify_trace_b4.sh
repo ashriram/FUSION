b589d2a8c2c3bdeb40d2f9e3a6c683774f92f394
Naveen Vedula <nvedula@sfu.ca>
2014-11-21 23:43:57 -0800

    Fixed protocol again

--- Traces/small/b4/modify_trace_b4.sh
+++ Traces/small/b4/modify_trace_b4.sh
@@ -1,12 +1,13 @@
-for x in {1..8}; do
+for x in {1..7}; do
     if [ ! -f "trace_${x}.raw" ]; then
-        #touch trace_${x}.raw
-        #gzip trace_${x}.raw
-        #mv trace_${x}.raw.gz trace_${x}.raw
-        cp ../trace.txt .
-        ln -s ../../../../tools/post-b4/process 
-        ./process 4096
-        rm process
+        touch trace_${x}.raw
+        gzip trace_${x}.raw
+        mv trace_${x}.raw.gz trace_${x}.raw
     fi
 done
+
+cp ../trace.txt .
+ln -s ../../../../tools/post-b4/process 
+./process 4096
+rm process
 
