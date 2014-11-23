12c00005e9e6b979cdbfb843c6461a025065b8eb
Naveen Vedula <nvedula@sfu.ca>
2014-11-23 02:52:42 -0800

    made changes to run-benchmark to wirte in csv

--- Run/run-benchmarks.sh
+++ Run/run-benchmarks.sh
@@ -17,21 +17,22 @@ do
         #echo "$(pwd)"
         cd ${APPS[$i]} 
         cwd=$(pwd)
-        echo "$cwd"
+        echo -e "$cwd">>../../../cycles.csv
+        #--- To Run BENCH MARK--------------------------
         #./ooo-sim>std_out  2>std_err &        
         #------ to Calculate CPU -- DMA --- ACC Cycles ----
         
-        grep "CPU halted after"  std_out | awk '{ sum+=$7} END {print sum}'
+        echo -e grep "CPU halted after"  std_out | awk '{ sum+=$7} END {print sum}'>>../../../cycles.csv
         if [ "${x}" == "b1"  ] 
         then
             DMA=`grep "DMA halted after"  std_out | awk '{ sum+=$7} END {print sum}'`
             DMA2=`echo "$DMA+40"|bc -l`
-            echo "$DMA2"
+            echo -e "$DMA2">>../../../cycles.csv
         else
-            echo  "0"
+            echo -e "0">>../../../cycles.csv
         fi
         
-        grep "ACC\s[2-7]\shalted after"  std_out | awk '{ sum+=$8} END {print sum}' 
+        grep "ACC\s[2-7]\shalted after"  std_out | awk '{ sum+=$8} END {print sum}'>>../../../cycles.csv 
         
         #-----------------------------------------------------
         cd ..
