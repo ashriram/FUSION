8f7c623dd485056007734fed6d46b2abba7a167f
Naveen Vedula <nvedula@sfu.ca>
2014-11-23 21:15:25 -0800

    adding Run folder

---
+++ Run/run-benchmarks-large.sh
@@ -0,0 +1,54 @@
+#!/bin/bash 
+
+APPS=('tracking' 'histogram'  'nw'  'disparity'  'susan'  'filter'  'fft' 'adpcm') 
+#APPS=('fft')
+#bench=('b1'  'b2'  'b3'  'b4')
+bench=('b1')
+
+path='large/'
+
+cd ${path}
+for x in ${bench[@]}
+do
+    cd ${x}    
+        
+    for(( i=0;i<${#APPS[@]};i++)) 
+    do
+        #echo "$(pwd)"
+        cd ${APPS[$i]} 
+        cwd=$(pwd)
+        echo -e "$cwd"
+        #--- To Run BENCH MARK--------------------------
+        #rm CONFIG_SCRATCHPAD_4K
+        #rm CONFIG       
+        #if [ "${x}" == "b1"  ] 
+        #then    
+        #    ln -s ../../../../gems-lib/ruby_clean/config/LARGE/CONFIG_SCRATCHPAD_8K  CONFIG
+        #else
+        #    ln -s ../../../../gems-lib/ruby_clean/config/LARGE/CONFIG_b2-b3-b4  CONFIG
+        #fi
+        #rm trace_file_list
+        #echo "1">trace_file_list
+        #echo "../../../../Traces/large/${x}/"${APPS[$i]}"/trace.txt">>trace_file_list
+        ./ooo-sim>std_out  2>std_err &        
+        #------ to Calculate CPU -- DMA --- ACC Cycles ----
+        
+        #grep "CPU halted after"  std_out | awk '{ sum+=$7} END {print sum}'
+        #
+        #if [ "${x}" == "b1"  ] 
+        #then
+        #    DMA=`grep "DMA halted after"  std_out | awk '{ sum+=$7} END {print sum}'`
+        #    DMA2=`echo "$DMA+40"|bc -l`
+        #    echo -e "$DMA2"
+        #else
+        #    echo -e "0"
+        #fi
+        #
+        #grep "ACC\s[2-7]\shalted after"  std_out | awk '{ sum+=$8} END {print sum}' 
+        
+        #-----------------------------------------------------
+        cd ..
+    done
+    cd ..
+done
+cd ..
