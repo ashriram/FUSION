593e05c6935415f8d339cae60f60c13711fdc9bd
Snehasish Kumar <ska124@sfu.ca>
2014-11-25 07:53:58 -0800

    removed mshrp

--- Run/mshrp-large/b1/fft/calc_cycles.sh
+++
@@ -1,5 +0,0 @@
-grep "CPU halted after"  std_out | awk '{ sum+=$7} END {print sum}'
-DMA=`grep "DMA halted after"  std_out | awk '{ sum+=$7} END {print sum}'`
-DMA2=`echo "$DMA+40"|bc -l`
-echo "$DMA2"
-grep "ACC\s[2-7]\shalted after"  std_out | awk '{ sum+=$8} END {print sum}' 
--- Run/mshrp-large/b1/nw/set_links.sh
+++
@@ -1,6 +0,0 @@
- ln -s ../../../../macsim/obj/ruby/Fusion/libruby.so 
- cp /home/nvedula/maxc/macsim/bin/params.in  .
- ln -s ../../../../gems-lib/ruby_clean/config/SMALL/CONFIG_SCRATCHPAD_4K CONFIG
- ln -s ../../../../gems-lib/ruby_clean/network
- ln -s ../../../../gems-lib/ruby_clean/config
- ln -s ../../../../macsim/bin/ooo-sim
